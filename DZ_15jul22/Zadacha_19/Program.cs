f4a0a7d84ea00c1c7ae4826829e2b8f41ad80363
AlekseyKav <nor-man@yandex.ru>
2022-07-26 20:54:21 +0300

    Add All

---
+++ DZ_15jul22/Zadacha_19/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+Console.Write("Введите пятизначное число: ");
+int num = 0; num = Convert.ToInt32(Console.ReadLine());
+
+int x5 = num / 10000;
+int x1 = num % 10;
+int x41 = num / 1000;
+int x4 = x41 % 10;
+int x2 = num / 10 % 10;
+
+/*for (int i = 0; i <= number ; i++)
+if (i % 2 == 0) Console.WriteLine(i);
+*/
+
+if (x5 == x1 && x4 == x2) Console.Write("число является палиндромом");
+else
+{
+    Console.Write("число НЕявляется палиндромом");
+}
---
+++ DZ_19jul22/Zadacha_27/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int getSumNumbs(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        int num1 = number % 10;
+        number = number / 10;
+        sum = sum + num1;
+    }
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(getSumNumbs(num));
