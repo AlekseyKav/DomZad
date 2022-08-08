fe84dbf078ca077d7b1170c6f1c3f8ceb25e3897
AlekseyKav <nor-man@yandex.ru>
2022-08-08 20:13:32 +0300

    add Z_41 & 43

---
+++ DZ_05aug22/ZADACHA_41/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Вам необходимо ввести произвольные, целые, положительные и/или отрицательные числа.\nКакое колличество чисел будем исследовать?\t");
+int sum = 0;
+int ElemCount = int.Parse(Console.ReadLine());
+int [] ar = new int[ElemCount];
+for (int i = 0; i < ElemCount; i++)
+{
+    Console.Write($"Введите число № {i + 1} -\t");
+    ar[i] = int.Parse(Console.ReadLine());
+}
+Console.Write("Из введнных Вами чисел ( ");
+foreach (int i in ar)
+Console.Write(i + ", ");
+
+for (int i = 0; i < ar.Length; i++)
+{
+      if (ar[i] > 0)
+      {
+        sum ++;
+      }
+}
+
+Console.WriteLine(") - положительными являются - " + sum + "  числа");
---
+++ DZ_05aug22/ZADACHA_43/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Для нахождения точки пересечения двух прямых заданных уравнениями");
+Console.WriteLine("y = k1 * x + b1, y = k2 * x + b2");
+Console.WriteLine("введите к1");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите b1");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите k2");
+double k2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите b2");
+double b2 = double.Parse(Console.ReadLine());
+
+double x = 0;
+double y = 0;
+x = (b2 - b1)/(k1 - k2);
+y = k1 * x + b1;
+
+Console.WriteLine($"Пересечения двух заданных прямых находится в точке с координатами X = {x} , Y = {y}");
