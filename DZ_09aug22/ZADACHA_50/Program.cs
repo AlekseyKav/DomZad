7567e539831032dc6945b24408da86dd45e7a26b
AlekseyKav <nor-man@yandex.ru>
2022-08-13 15:29:54 +0300

    Zaliv DZ_09aug22

---
+++ DZ_09aug22/ZADACHA_50/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// 17 -> числа с такой позицией в массиве нет.
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max);
+          }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+ 
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+ 
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+ 
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+Console.Write("Введите номер строки элемента: ");
+int ro = int.Parse(Console.ReadLine());
+
+Console.Write("Введите номер столбца элемента: ");
+int cl = int.Parse(Console.ReadLine());
+
+if (ro > array.GetLength(0) || cl > array.GetLength(1))
+    Console.Write($"Элемент с такой позицией в массиве отсутствует.");
+else
+{
+    Console.Write($"На заданной позиции находится число - {array[ro-1, cl-1]} ");   
+}
+
---
+++ DZ_09aug22/ZADACHA_52/Program.cs
@@ -0,0 +1,46 @@
+﻿int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+
+for (int i = 0; i < columns; i++)
+{
+    double summ = 0;
+
+    for (int j = 0; j < rows; j++)
+    {
+        summ += array[j, i];
+    }
+    double res = Math.Round (summ / rows,2);
+    Console.Write($"{res}\t");
+}
